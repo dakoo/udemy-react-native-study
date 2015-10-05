4bc286d2d3717ebae97d4a5984797fd6b46e5e29
Hochul Shin <darkblue99@gmail.com>
2015-10-06 00:50:48 +0900

    fin

--- weekday/index.ios.js
+++ weekday/index.ios.js
@@ -1,40 +1,43 @@
-//Import some code we need
+// Import some code we need
+var Moment = require('moment');
 var React = require('react-native');
-var AppRegistry = React.AppRegistry; 
+var AppRegistry = React.AppRegistry;
 var Text = React.Text;
 var View = React.View;
 var StyleSheet = React.StyleSheet;
 var DayItem = require('./src/day-item');
 
-var DAYS = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-//Create a react component
+// Create a react component
 var Weekdays = React.createClass({
-  render: function(){
-    return  <View style={styles.container}>
-      <Text>
-          Days of the week:
-      </Text>
+  render: function() {
+    return <View style={styles.container}>
       {this.days()}
     </View>
   },
-  days:function(){
-      return DAYS.map(function(day){
-        //called 7 times, one for each day fo the week
-        return <DayItem day={day} />
-      });
+  days: function() {
+    var daysItems = [];
+
+    for(var i = 0; i < 7; i++){
+      var day = Moment().add(i, 'days').format('dddd');
+      daysItems.push(
+        <DayItem day={day} daysUntil={i} />
+      )
+    }
+
+    return daysItems
   }
 });
 
-//Style the React component
+// Style the React component
 var styles = StyleSheet.create({
   container: {
-    flex:1,
-    justifyContent:'center', //Move stuff height wise
-    alignItems: 'center'    //Move stuff width wise
-  } 
+    flex: 1,
+    justifyContent: 'center', // Moves stuff height wise
+    alignItems: 'center' // Moves stuff width wise
+  }
 });
 
-//Show the react component on the scree
-AppRegistry.registerComponent('weekdays', function(){
+// Show the react component on the screen
+AppRegistry.registerComponent('weekdays', function() {
   return Weekdays
-})
+});
--- weekday/src/day-item.js
+++ weekday/src/day-item.js
@@ -1,26 +1,37 @@
-//Import some code we need
+// Import some code we need
 var React = require('react-native');
 var Text = React.Text;
-var StyleSheet = React.StyleSheet;
 
-//Create our component
+// Create our component
 var DayItem = React.createClass({
-	render: function() {
-		return (
-			<Text style={styles.day}>
-				{this.props.day}
-			</Text>
-		);
-	}
-})
-
-//Style our component
-var styles = StyleSheet.create({
-	day: {
-		fontSize: 18, 
-		color: '#0000FF'
-	}
+  render: function() {
+    return <Text style={this.style()}>
+      {this.props.day}
+    </Text>
+  },
+  style: function() {
+    return {
+      color: this.color(),
+      // fontWeight: this.fontWeight(),
+      fontSize: this.fontSize(),
+      lineHeight: this.lineHeight()
+    }
+  },
+  color: function() {
+    var opacity = 1 / this.props.daysUntil;
+    return 'rgba(0, 0, 0, '+ opacity + ')';
+  },
+  fontWeight: function() {
+    var weight = 7 - this.props.daysUntil;
+    return weight * 100;
+  },
+  fontSize: function() {
+    return 60 - 6 * this.props.daysUntil;
+  },
+  lineHeight: function() {
+    return 70 - 4 * this.props.daysUntil;
+  }
 });
 
-//Make this code available elsewhere
+// Make this code available elsewhere
 module.exports = DayItem;
